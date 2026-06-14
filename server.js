35f629c610ce4155d2ef0f1124c8b76e0848df22
Garencito <garencito@localhost>
2026-06-14 00:30:04 -0600

    Fix server.js to strip query strings from static file paths

--- server.js
+++ server.js
@@ -70,7 +70,8 @@ const server = http.createServer((req, res) => {
   }
 
   // Static files
-  let filePath = path.join(__dirname, req.url === '/' ? 'index.html' : req.url);
+  const urlPath = req.url.split('?')[0];
+  let filePath = path.join(__dirname, urlPath === '/' ? 'index.html' : urlPath);
   const ext = path.extname(filePath).toLowerCase();
 
   fs.readFile(filePath, (err, data) => {
